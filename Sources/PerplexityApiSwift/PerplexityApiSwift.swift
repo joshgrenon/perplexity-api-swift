0a7cdf012cecb6645d9760da37c8b0619262cc27
Josh <joshgrenon@users.noreply.github.com>
2025-03-11 17:05:45 -0700

    Updates enums to support the latest Perplexity api models (#1)

--- Sources/PerplexityApiSwift/PerplexityApiSwift.swift
+++ Sources/PerplexityApiSwift/PerplexityApiSwift.swift
@@ -9,7 +9,7 @@ public class PerplexityApiSwift {
         self.bearerToken = token
     }
     
-    public func chatCompletion(messages: [Message], model: PerplexityModel = .sonarLargeOnline) async throws -> PerplexityResponse {
+    public func chatCompletion(messages: [Message], model: PerplexityModel = .sonar) async throws -> PerplexityResponse {
         guard let bearerToken = bearerToken else {
             throw PerplexityError.tokenNotSet
         }
